2dfb323f4dc1aeb6b0c2d5d09bb95ce29fa6dc18
한창희 <nimok97@gmail.com>
2022-07-06 21:59:20 +0900

    feat : 화면 이동 및 데이터 전달 구현 메인액티비티로 이동 시 플래그값 할당해 액티비티 스택 비움 닉네임, 이메일 값을 담은 Information data class 전달

--- app/src/main/java/com/nimok97/mailproject/ui/MainActivity.kt
+++ app/src/main/java/com/nimok97/mailproject/ui/MainActivity.kt
@@ -1,18 +1,27 @@
 package com.nimok97.mailproject.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.nimok97.mailproject.R
+import com.nimok97.mailproject.common.PrintLog
+import com.nimok97.mailproject.data.Information
 import com.nimok97.mailproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private  val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val information = intent.getSerializableExtra("information") as Information
+        PrintLog.printLog("$this / onCreate, nickName : ${information.nickName}, email : ${information.email}")
+        viewModel.information = information
+
 
 
     }
---
+++ app/src/main/java/com/nimok97/mailproject/ui/MainViewModel.kt
@@ -0,0 +1,10 @@
+package com.nimok97.mailproject.ui
+
+import androidx.lifecycle.ViewModel
+import com.nimok97.mailproject.data.Information
+
+class MainViewModel : ViewModel() {
+
+    lateinit var information : Information
+
+}
