c5791dc9b51b22fb8d53642f095c996617296a24
한창희 <nimok97@gmail.com>
2022-07-08 02:17:06 +0900

    feat : 태블릿 화면 대응 가로 길이 600dp 이상인 경우 가로모드 - navigation rail 세로모드 시 - BottomNavigaionView  나타나도록 구현 회전 시 선택된 탭 유지 구현

--- app/src/main/java/com/nimok97/mailproject/ui/MainActivity.kt
+++ app/src/main/java/com/nimok97/mailproject/ui/MainActivity.kt
@@ -31,13 +31,22 @@ class MainActivity : AppCompatActivity() {
         viewModel.information = information
 
         setBottomNavigationView()
+        setNavigationRailView()
         initView()
     }
 
     private fun initView() {
-        when(viewModel.bottonNavigaionFragment){
-            BottomNavigationFragmentType.MAIL -> changeFragment(mailFragment)
-            BottomNavigationFragmentType.SETTING -> changeFragment(settingFragment)
+        when (viewModel.bottonNavigaionFragment) {
+            BottomNavigationFragmentType.MAIL -> {
+                changeFragment(mailFragment)
+                binding.bottomNavigationViewMain?.menu?.getItem(0)?.isChecked = true
+                binding.navigationRailViewMain?.menu?.getItem(0)?.isChecked = true
+            }
+            BottomNavigationFragmentType.SETTING -> {
+                changeFragment(settingFragment)
+                binding.bottomNavigationViewMain?.menu?.getItem(1)?.isChecked = true
+                binding.navigationRailViewMain?.menu?.getItem(1)?.isChecked = true
+            }
         }
 
 //        changeFragment(mailFragment)
@@ -48,7 +57,29 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setBottomNavigationView() {
-        binding.bottomNavigationViewMain.run {
+        binding.bottomNavigationViewMain?.run {
+            setOnItemSelectedListener {
+                when (it.itemId) {
+                    R.id.fragment_mail -> {
+                        clearBackStack()
+                        changeFragment(mailFragment)
+                    }
+                    R.id.fragment_search -> {
+                        //changeFragment(settingFragment)
+                        //clearBackStack()
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.fragmentContainerView_main, settingFragment, "Setting")
+                            .addToBackStack("Setting")
+                            .commit()
+                    }
+                }
+                true
+            }
+        }
+    }
+
+    private fun setNavigationRailView(){
+        binding.navigationRailViewMain?.run {
             setOnItemSelectedListener {
                 when (it.itemId) {
                     R.id.fragment_mail -> {
@@ -79,17 +110,34 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onBackPressed() {
-        if (binding.bottomNavigationViewMain.selectedItemId == R.id.fragment_search) { // 현재 탭이 Setting
-            binding.bottomNavigationViewMain.selectedItemId = R.id.fragment_mail
-        } else { // 현재 탭이 Mail
-            mailFragment?.let{
-                PrintLog.printLog("${it.childFragmentManager.backStackEntryCount}")
+        binding.bottomNavigationViewMain?.let {
+            if (binding.bottomNavigationViewMain?.selectedItemId == R.id.fragment_search) { // 현재 탭이 Setting
+                binding.bottomNavigationViewMain?.selectedItemId = R.id.fragment_mail
+            } else { // 현재 탭이 Mail
+                mailFragment?.let {
+                    PrintLog.printLog("${it.childFragmentManager.backStackEntryCount}")
 
-                if(it.childFragmentManager.backStackEntryCount >= 1){
-                    it.backToPrimary()
+                    if (it.childFragmentManager.backStackEntryCount >= 1) {
+                        it.backToPrimary()
+                    } else {
+                        super.onBackPressed()
+                    }
                 }
-                else{
-                    super.onBackPressed()
+            }
+        }
+
+        binding.navigationRailViewMain?.let {
+            if (binding.navigationRailViewMain?.selectedItemId == R.id.fragment_search) { // 현재 탭이 Setting
+                binding.navigationRailViewMain?.selectedItemId = R.id.fragment_mail
+            } else { // 현재 탭이 Mail
+                mailFragment?.let {
+                    PrintLog.printLog("${it.childFragmentManager.backStackEntryCount}")
+
+                    if (it.childFragmentManager.backStackEntryCount >= 1) {
+                        it.backToPrimary()
+                    } else {
+                        super.onBackPressed()
+                    }
                 }
             }
         }
